1e916ff657d31e43f8e5e6b921b711b9a7d5a69c
Cahian Freire <cahianfreire@gmail.com>
2022-04-07 12:17:58 -0300

    Exercise 4: Add insertion sort

--- exercise4.js
+++ exercise4.js
@@ -1,9 +1,22 @@
 // Height Checker
 // https://leetcode.com/problems/height-checker/
 
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i += 1) {
+    const val = array[i];
+
+    let j = i;
+    while (j > 0 && val < array[j - 1]) {
+      array[j] = array[j - 1];
+      j -= 1;
+    }
+    array[j] = val;
+  }
+};
+
 const heightChecker = (heights) => {
   const expected = [...heights];
-  expected.sort((a, b) => a - b);
+  insertionSort(heights);
 
   let notMatch = 0;
   for (let i = 0; i < heights.length; i += 1) {
